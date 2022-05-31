099ee5284c66181ada859289c716d58a314552a6
D4rk5t0rM <github.pianism@slmail.me>
2022-05-31 17:07:18 +0200

    added colours to display CVE-2022-30190 more clearly.

--- macro.sh
+++ macro.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+
+
 echo "Oleid:"
 echo "=========="
 echo
@@ -20,9 +34,13 @@ echo "++++++++++"
 echo "Checking for possible MSDT CVE-2022-30190 - follina:"
 echo "=========="
 echo
-zipgrep -i '''Target="http''' $1 | sed 's/ /\n/g' | grep -i '''Target="h'''
+output=$(zipgrep -i '''Target="http''' $1 | sed 's/ /\n/g' | grep -i '''Target="h''')
+echo -e "${red}${output}${reset}"
 wget $(zipgrep -i '''Target="http''' 4a24048f81afbe9fb62e7a6a49adbd1faf41f266b5f9feecdceb567aec096784 | sed 's/ /\n/g' | grep -i '''Target="h''' | awk -F '"' '{print $2}' | sed -E 's/!$//g' ) CVE-2022-30190.html.txt
-grep -i "ms-msdt" CVE-2022-30190.html.txt
+echo "-"
+output=$(grep -i "ms-msdt" CVE-2022-30190.html.txt)
+echo -e "${red}${output}${reset}"
+echo "-"
 
 echo "++++++++++"
 echo "mraptor:"
