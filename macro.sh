9e018f5377f11e30ed55a6128101716d8eb9ee31
D4rk5t0rM <github.pianism@slmail.me>
2022-05-31 16:17:17 +0200

    add parser for the downloaded HTML file

--- macro.sh
+++ macro.sh
@@ -21,6 +21,8 @@ echo "Checking for possible MSDT CVE-2022-30190 - follina:"
 echo "=========="
 echo
 zipgrep -i '''Target="http''' $1 | sed 's/ /\n/g' | grep -i '''Target="h'''
+wget $(zipgrep -i '''Target="http''' 4a24048f81afbe9fb62e7a6a49adbd1faf41f266b5f9feecdceb567aec096784 | sed 's/ /\n/g' | grep -i '''Target="h''' | awk -F '"' '{print $2}' | sed -E 's/!$//g' ) CVE-2022-30190.html.txt
+grep -i "ms-msdt" CVE-2022-30190.html.txt
 
 echo "++++++++++"
 echo "mraptor:"
