7971dc43d644aae0f7b5b55e80eaf7f4a13b87ee
D4rk5t0rM <github.pianism@slmail.me>
2022-10-24 15:02:07 +0000

    Update macro.sh

--- macro.sh
+++ macro.sh
@@ -56,7 +56,7 @@ echo "=========="
 echo
 output=$(zipgrep -i '''Target="http''' $1 | sed 's/ /\n/g' | grep -i '''Target="h''')
 echo -e "${red}${output}${reset}"
-wget $(zipgrep -i '''Target="http''' 4a24048f81afbe9fb62e7a6a49adbd1faf41f266b5f9feecdceb567aec096784 | sed 's/ /\n/g' | grep -i '''Target="h''' | awk -F '"' '{print $2}' | sed -E 's/!$//g' ) CVE-2022-30190.html.txt
+wget $(zipgrep -i '''Target="http''' $1 | sed 's/ /\n/g' | grep -i '''Target="h''' | awk -F '"' '{print $2}' | sed -E 's/!$//g' ) CVE-2022-30190.html.txt
 echo "-"
 output=$(grep -i "ms-msdt" CVE-2022-30190.html.txt)
 echo -e "${red}${output}${reset}"
